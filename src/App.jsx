da04a3993ef2c18c3eb2cb9b56e81257d7a0b27c
10urkarasu <10urkarasu@gmail.com>
2024-04-09 21:54:34 +0300

    added some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+
 function App() {
-    return <div></div>;
+    return (
+        <>
+            <GlobalStyles />
+            <div>
+                <Heading as="h1">Test</Heading>
+                <Heading as="h2">Test</Heading>
+                <Heading as="h3">Test</Heading>
+            </div>
+        </>
+    );
 }
 
 export default App;
