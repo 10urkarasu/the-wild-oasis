a63f3365bf692f9008b1f55191eb2d5032b4912e
10urkarasu <10urkarasu@gmail.com>
2024-04-14 22:51:12 +0300

    added react hook form

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,6 +9,18 @@ export async function getCabins() {
     return cabins;
 }
 
+export async function createCabin(newCabin) {
+    const { data, error } = await supabase
+        .from("cabins")
+        .insert([newCabin])
+        .select();
+    if (error) {
+        console.error(error);
+        throw new Error("Cabins could not be deleted");
+    }
+    return data;
+}
+
 export async function deleteCabin(id) {
     const { error, cabins } = await supabase
         .from("cabins")
